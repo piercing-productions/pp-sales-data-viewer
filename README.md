c7afd82a3e89d73697b61b895632f617f1b504df
jpmclaughlin <jasonpmclaughlin@gmail.com>
2015-07-30 17:18:57 -0500

    first real commit
    baby steps

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
 # SalesDataViewer
-alpha testing
+
+## Overview
+
+This version of the Angular JS application was forked from the angular-seed.  It displays data based on user input.  Ultimately a table of data is displayed.
+
+
+## Prerequisites
+
+* [Git][git]
+* [Node.js][node]
+* [NPM.js][npm]
+* Install the tool dependencies (`npm install`)
+
+
+## Workings of the application
+
+* The application filesystem layout structure is based on the [angular-seed] project.
+* There is no dynamic backend (no application server) for this application. Instead we fake an application server by fetching static json files.
+
+
+## Development environment
+
+The following describes how you can develop, test, and deploy this application.
+
+### Installing dependencies
+
+There are two kinds of dependencies in this project: tools and angular framework code.  The tools help manage and test the application.
+
+* Get the tools we depend upon via `npm`, the [node package manager][npm].
+* Get the angular code via `bower`, a [client-side code package manager][bower].
+
+To install, simply call:
+
+```
+npm install
+```
+
+Behind the scenes this will also call `bower install`.  You should find that you have two new
+folders in your project.
+
+* `node_modules` - contains the npm packages for the tools we need
+* `bower_components` - contains the angular framework files
+
+### Run the Application
+
+The project is preconfigured with a simple development web server.  The simplest way to start this server is:
+
+```
+grunt serve
+```
+
+It should automatically launch the application in your default browser.  Otherwise, view it at `http://localhost:9000/`.
+
+
+## Building the Application Files
+
+You can build an optimized deployment package into a dist folder by calling:
+
+```
+grunt build
+```
+
+
+## Deploying/Pushing the Application Files
+
+I've disabled the capability to deploy for now.  Otherwise you could simply call:
+
+```
+grunt push
+```
+
+
+## Updating Angular
+
+Previously we recommended that you merge in changes to angular-seed into your own fork of the project.
+Now that the angular framework library code and tools are acquired through package managers (npm and
+bower) you can use these tools instead to update the dependencies.
+
+You can update the tool dependencies by running:
+
+```
+npm update
+```
+
+This will find the latest versions that match the version ranges specified in the `package.json` file.
+
+You can update the Angular dependencies by running:
+
+```
+bower update
+```
+
+This will find the latest versions that match the version ranges specified in the `bower.json` file.
+
+
+[git]: http://git-scm.com/
+[bower]: http://bower.io
+[npm]: https://www.npmjs.org/
+[node]: http://nodejs.org
+[protractor]: https://github.com/angular/protractor
+[jasmine]: http://jasmine.github.io
+[karma]: http://karma-runner.github.io
+[travis]: https://travis-ci.org/
+[http-server]: https://github.com/nodeapps/http-server
---
+++ app/scripts/app.js
@@ -0,0 +1,138 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ppBaseAppSeedApp
+ * @description
+ * # ppBaseAppSeedApp
+ *
+ * Main module of the application.
+ */
+
+var salesApp = angular.module('salesApp', ['ui.bootstrap', 'googlechart']);
+
+salesApp.controller('listCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $http.get('data/sales.json').success(function(data) {
+    $scope.raw = data;
+    $scope.customers = [];
+    $scope.selectedCustomer = {};
+    $scope.years = ['2010', '2011', '2012', '2013', '2014', '2015'];
+    $scope.curTonsData = [0, 0, 0, 0, 0, 0];
+    $scope.resetTonsData = function() {
+      $scope.curTonsData = [0, 0, 0, 0, 0, 0];
+    };
+    // $scope.resetTonsData();
+
+    $scope.calcTotalTons = function() {
+      $scope.resetTonsData();
+      for (var i = 0; i < $scope.raw.length; i++) {
+        if ($scope.raw[i].cus_no.trim() === $scope.selectedCustomer) {
+          for (var j = 0; j < $scope.years.length; j++) {
+            var n = $scope.raw[i][$scope.years[j]];
+            if (!isNaN(parseFloat(n))) {
+              $scope.curTonsData[j] += parseInt(n);
+            }
+          }
+        }
+      }
+      // console.log($scope.curTonsData);
+    };
+
+    // get unique list of customers (cus_no)
+    for (var i = 0; i < $scope.raw.length; i++) {
+      $scope.customers.push($scope.raw[i].cus_no.trim());
+    }
+    $scope.customers = $.unique( $scope.customers );
+
+    // ... to autocomplete in the search input
+    $( '#tags' ).autocomplete({ source: $scope.customers });
+
+  });
+
+  // $scope.showSelected = function(q) {
+  //   //alert('starting showSelected, q = ' + q + ', raw.length = ' + $scope.raw.length);
+  //   for (var i = 0; i < $scope.raw.length; i++) {
+  //     if ($scope.raw[i].cus_no.trim() === q) {
+  //       for (var j = 0; j < $scope.raw[i].shipTos.length; i++) {
+  //         console.log($scope.raw[i].shipTos[j].tons[0]);
+  //       }
+  //     }
+  //   }
+  // };
+
+  // SELECT CUSTOMER
+  // $scope.selectCustomer = function() {
+  //   // cache array of nodes
+  //
+  //   $scope.selectedCustomer =
+  //   //
+  // };
+
+  // RENDER
+
+
+  $(':submit').click(function() {
+    if ($('#tags').val() != $scope.selectedCustomer) {
+      $scope.$apply(function() {
+        // sync up vars
+        $scope.query = $scope.selectedCustomer = $('#tags').val();
+        // get tons for selected customer
+        $scope.calcTotalTons();
+        // update the chart
+        $scope.chartObject.data = [
+          ['Year', 'Tons'],
+          [$scope.years[0], $scope.curTonsData[0]],
+          [$scope.years[1], $scope.curTonsData[1]],
+          [$scope.years[2], $scope.curTonsData[2]],
+          [$scope.years[3], $scope.curTonsData[3]],
+          [$scope.years[4], $scope.curTonsData[4]],
+          [$scope.years[5] + ' YTD', $scope.curTonsData[5]]
+        ];
+      });
+    }
+  });
+
+  $scope.chartObject = {};
+
+  // $scope.chartObject.data = {'cols': [
+  //     {id: 't', label: 'Year', type: 'string'},
+  //     {id: 's', label: 'Tons', type: 'number'}
+  // ], 'rows': [
+  //     {c: [
+  //         {v: '2010'},
+  //         {v: 300},
+  //     ]},
+  //     {c: [
+  //         {v: '2011'},
+  //         {v: 275},
+  //     ]},
+  //     {c: [
+  //         {v: '2012'},
+  //         {v: 178}
+  //     ]},
+  //     {c: [
+  //         {v: '2013'},
+  //         {v: 489},
+  //     ]},
+  //     {c: [
+  //         {v: '2014'},
+  //         {v: 267},
+  //     ]},
+  //     {c: [
+  //         {v: '2015'},
+  //         {v: 679},
+  //     ]}
+  // ]};
+
+  $scope.chartObject.type = 'ColumnChart';
+  $scope.chartObject.options = {
+    height: 300,
+    colors: ['#1b9e77'],
+    chartArea: {width: '80%'},
+    bar: { groupWidth: '75%' },
+    legend: { position: 'none' },
+    vAxis: { minValue: 0 }
+  };
+
+}]);
